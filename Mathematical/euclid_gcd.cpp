e71f93e3f55f2b81785cb9922db61d10cd31e364
megham-garg <gargmegham3@gmail.com>
2021-01-24 11:42:48 +0530

    new commit

--- Mathematical/euclid_gcd.cpp
+++ Mathematical/euclid_gcd.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 typedef long long ll;
 #define MOD (pow(10,9)+7)
+
+class Triplet{
+public:
+    int x,y,gcd;
+};
+
+Triplet extended_euclid(int a, int b){
+    Triplet smallAns = extended_euclid(b, a%b);
+    Triplet myAns;
+    myAns.gcd = smallAns.gcd;
+    myAns.x = smallAns.y;
+    myAns.y = smallAns.x - (a/b)*smallAns.y
+    return Triplet;
+}
+
 int GCD(int a, int b){
     if(a<b) return GCD(b,a);
     if(!b) return a;
@@ -17,3 +32,6 @@ int main()
 // a%b = a - b*(a/b)
 // ax + by = GCD(a,b)
 // GCD(a,b) = GCD(b, a%b) for a>b
+// therefore bx1 + (a%b)y1 = ax + by
+// x = y1
+// y = x1 - (a/b)y1
