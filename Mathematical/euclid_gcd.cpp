17c71b74af1badad77014cd69d7d4839c36d71f5
megham-garg <gargmegham3@gmail.com>
2021-01-24 09:58:05 +0530

    new commit

---
+++ Mathematical/euclid_gcd.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define MOD (pow(10,9)+7)
+int GCD(int a, int b){
+    if(a<b) return GCD(b,a);
+    if(!b) return a;
+    return GCD(b, a%b);
+}
+int main()
+{
+    cout << gcd(30, 15);
+    return 0;
+}
