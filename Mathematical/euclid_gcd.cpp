bb11e4ebc64298ae14b6a00ccc60f9e42ef7cebb
megham-garg <gargmegham3@gmail.com>
2021-01-24 11:36:31 +0530

    new commit

--- Mathematical/euclid_gcd.cpp
+++ Mathematical/euclid_gcd.cpp
@@ -7,9 +7,13 @@ int GCD(int a, int b){
     if(!b) return a;
     return GCD(b, a%b);
 }
+
 int main()
 {
     cout << gcd(30, 15);
     return 0;
 }
 // O(log(max(a,b)))
+// a%b = a - b*(a/b)
+// ax + by = GCD(a,b)
+// GCD(a,b) = GCD(b, a%b) for a>b
