af95addacbbad0ec3c98193bf90cfa166de5d61b
megham-garg <gargmegham3@gmail.com>
2021-01-25 12:25:35 +0530

    new

--- AlgoExpert/moveElementToEnd.cpp
+++ AlgoExpert/moveElementToEnd.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// O(n) time
+// O(1) space
 void moveElementToEnd(vector<int> &arr, int target){
     int left = 0;
     int right = arr.size()-1;
@@ -16,7 +18,7 @@ void moveElementToEnd(vector<int> &arr, int target){
 int main(){
     vector<int> arr;
     arr.assign({2,1,2,2,2,3,4,1,2});
-    moveElementToEnd(arr, 2);
+    moveElementToEnd(arr, 1);
     for(auto num : arr) cout << num << " ";
     cout << endl;
     return 0;
