4bd77d26c81fbc98133c12300b511a20d6fdf065
megham-garg <gargmegham3@gmail.com>
2021-01-25 15:34:13 +0530

    new

---
+++ AlgoExpert/longestPeak.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// O(n) time
+// O(1) space
+void longestPeak(vector<int> arr){
+    int n = arr.size();
+    if(n<3) return;
+    int maxL = 0, maxPeak = -1;
+    int leftTail = 0, rightTail = 0, curPeak = -1;
+    for(int i = 1; i < n-1; i++){
+        int prev = arr[i-1];
+        int cur = arr[i];
+
+        if(cur > prev){
+            if(curPeak == -1){leftTail++;}
+            else{
+                if(maxL < leftTail+1+rightTail){
+                    maxPeak = curPeak;
+                    maxL = max(maxL, leftTail+1+rightTail);
+                }
+                leftTail = 1;
+                rightTail = 0;
+                curPeak = -1;
+            }
+        }
+        else if(cur < prev) {
+            if(curPeak==-1) curPeak = i-1;
+            rightTail++;
+        }
+        else if(cur == prev && curPeak!=-1){
+            if(maxL < leftTail+1+rightTail){
+                maxPeak = curPeak;
+                maxL = max(maxL, leftTail+1+rightTail);
+            }
+        }
+        else if(cur == prev && curPeak==-1){
+            leftTail = 0;
+            rightTail = 0;
+            curPeak = -1;
+        }
+    }
+    std::cout << "max length of peak is: "  << maxL << " at: " << arr[maxPeak] << "\n";
+}
+
+int main(){
+    vector<int> arr;
+    arr.assign({1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3});
+    longestPeak(arr);
+    return 0;
+}
