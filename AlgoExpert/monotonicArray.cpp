be8c7ddedadc1c6784f07fa13db6ea5fe23f2b63
megham-garg <gargmegham3@gmail.com>
2021-01-25 14:21:14 +0530

    new

--- AlgoExpert/monotonicArray.cpp
+++ AlgoExpert/monotonicArray.cpp
@@ -1,12 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// O(n) time
+// O(1) space
 bool isMonotonic(vector<int> arr){
     int curSmall = arr[0]; //assume smallest
     int changes = 0;
     int n = arr.size();
     for(int i = 1; i < n; i++){
-        if(arr[i] < curSmall) {
+        if(arr[i] <= curSmall) {
             curSmall = arr[i];
             changes++;
         }
@@ -20,7 +22,7 @@ int main(){
     cout << isMonotonic(arr) << "\n";
     arr.assign({-1, -5, -1100, -1102});
     cout << isMonotonic(arr) << "\n";
-    arr.assign({-1, -5, -1100, -1100, -1101, -1002});
+    arr.assign({-1, -5, -1100, -1100, -1101, -1102});
     cout << isMonotonic(arr) << "\n";
     arr.assign({1, 5, 12, 16552});
     cout << isMonotonic(arr) << "\n";
