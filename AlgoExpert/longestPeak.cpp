167af2c3df9b472312b4ca9b8c2a42feebd12b65
megham-garg <gargmegham3@gmail.com>
2021-01-25 16:12:28 +0530

    new

--- AlgoExpert/longestPeak.cpp
+++ AlgoExpert/longestPeak.cpp
@@ -9,7 +9,8 @@ void longestPeak(vector<int> arr){
     if(n<3) return;
     int maxL = 0, maxPeak = -1;
     int leftTail = 0, rightTail = 0, curPeak = -1;
-    for(int i = 1; i < n-1; i++){
+    for(int i = 1; i < n; i++){
+        cout << leftTail << " "  << rightTail << " " << arr[curPeak] << "\n";/
         int prev = arr[i-1];
         int cur = arr[i];
 
@@ -28,6 +29,10 @@ void longestPeak(vector<int> arr){
         else if(cur < prev) {
             if(curPeak==-1 && leftTail) curPeak = i-1;
             rightTail++;
+            if(i == n-1 && curPeak!=-1 && maxL < leftTail+1+rightTail){
+                maxPeak = curPeak;
+                maxL = max(maxL, leftTail+1+rightTail);
+            }
         }
         else{
             if(curPeak!=-1 && maxL < leftTail+1+rightTail){
@@ -44,7 +49,8 @@ void longestPeak(vector<int> arr){
 
 int main(){
     vector<int> arr;
-    arr.assign({1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3});
+    // arr.assign({1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3});
+    arr.assign({1, 3, 1, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5});
     longestPeak(arr);
     return 0;
 }
