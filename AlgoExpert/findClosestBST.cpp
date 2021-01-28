94f21fe1e2c6a83ffba242d11f96b22ca4a49237
megham-garg <gargmegham3@gmail.com>
2021-01-28 08:41:16 +0530

    findClosestBST

---
+++ AlgoExpert/findClosestBST.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+int findClosestInBST(Node* node, int target){
+    if(!node) return INT_MAX;
+    if(node->data == target) return target;
+    Node* next = NULL;
+
+    if(node->left && node->data > target) next = node->left;
+    else if(node->right && node->data < target) next = node->right;
+    else return node->data;
+    
+    int fut = findClosestInBST(next, target);
+    return (abs(node->data - target) < abs(fut - target))?node->data:fut;
+}
+
+int main(){
+    return 0;
+}
