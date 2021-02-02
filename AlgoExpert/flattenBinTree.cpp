1379b2e068f246dae59a23c645243b32ae9151e9
megham-garg <gargmegham3@gmail.com>
2021-02-02 10:33:58 +0530

    new code

---
+++ AlgoExpert/flattenBinTree.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void FTHelper(Node* cur, Node* prev){
+    if(cur->left) FTHelper(cur->left, prev);
+    cur->left = prev;
+    if(prev) prev->right = cur;
+    prev = cur;
+    if(cur->right) FTHelper(cur->right, prev);
+}
+
+void flattenTree(Node* node){
+    Node* prev = NULL;
+    FTHelper(root, prev);
+}
