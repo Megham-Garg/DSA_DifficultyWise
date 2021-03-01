034490e42fc49498470fecbee6e4c280b52a175e
megham-garg <gargmegham3@gmail.com>
2021-03-01 10:27:11 +0530

    LRU Cache

--- AlgoExpert/LRU_Cache.cpp
+++ AlgoExpert/LRU_Cache.cpp
@@ -11,25 +11,55 @@ struct Node{
 };
 
 class DLL{
-    Node* head,tail;
+    Node* head;
+    Node* tail;
 public:
-    void shiftToEnd(){}
-    void remove_front(){}
-    void append(){}
-    void get_head(){}
+    void shiftToEnd(Node* target){
+        if(target == tail) return;
+        Node node;
+        node.key = target->key;
+        node.value = target->value;
+        node.next = NULL;
+        node.prev = tail;
+        tail = &node;
+        if(target->next){
+            target->key = target->next->key;
+            target->value = target->next->value;
+            if(target->next->next) target->next->prev = target;
+            target->next = target->next->next;
+        }
+    }
+    char remove_front(){
+        char key;
+        if(head){
+            key = head->key;
+            head = head->next;
+            head->prev = NULL;
+        }
+        return key;
+    }
+    Node* append(char key, int val){
+        Node node;
+        node.key = key;
+        node.value = val;
+        node.prev = tail;
+        node.next = NULL;
+        tail = &node;
+    }
+    Node* get_head() {return head;}
 };
 
 // O(1) space
 // O(1) time
 
 class LRU_Cache{
-    DLL<char> cache;
+    DLL cache;
     unordered_map<char, Node*> hash;
 public:
     void insert(char key, int value){
         if(hash.size() >= 3){
             if(hash.find(key) != hash.end()){
-                cache.shiftToEnd(key);
+                cache.shiftToEnd(hash[key]);
             }
             else{
                 char key = cache.remove_front();
@@ -41,7 +71,7 @@ public:
 
     unordered_map<char, int> get_LRUData(){
         unordered_map<char, int> res;
-        if(cache.size() > 0){
+        if(hash.size() > 0){
             Node* front = cache.get_head();
             res[front->key] = front->value;
         }
@@ -50,7 +80,7 @@ public:
 
     int get_value(char key){
         Node* target;
-        if(cache.size() > 0){
+        if(hash.size() > 0){
             if(hash.find(key) != hash.end()) target = hash[key];
             else return INT_MIN;
             cache.shiftToEnd(target);
